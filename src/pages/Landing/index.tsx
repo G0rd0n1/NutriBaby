03e559cd39fdd86a627130b188b008192dc19ef0
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-08-27 22:27:35 +0200

    Finish prelim landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,10 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 const Landing = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <Text>This is the Landing page</Text>
+      <View style={styles.hero}>
+        <Text allowFontScaling={false} selectable={false} style={styles.logo}>
+          NutriLearn
+        </Text>
+        <Text
+          allowFontScaling={false}
+          selectable={false}
+          style={styles.description}
+        >
+          The free, fun, and effective way to learn about food and
+          nutrition!
+        </Text>
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={()=>alert("Pressed")} style={styles.button}>
+          <Text style={styles.buttonText}>Get started</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
--- src/pages/Landing/styles/index.ts
+++ src/pages/Landing/styles/index.ts
@@ -1,5 +1,54 @@
 import { StyleSheet } from "react-native";
+import {heightPercentageToDP, widthPercentageToDP} from "react-native-responsive-screen";
 
 export const styles = StyleSheet.create({
-    container: {},
+    container: {
+        marginVertical: heightPercentageToDP("2"),
+    },
+    logo: {
+        fontFamily: "Lobster",
+        fontSize: widthPercentageToDP("3"), 
+        fontWeight: "600",
+        flexDirection: "column",
+        alignSelf: "center",
+        marginBottom: heightPercentageToDP("5"),
+    },
+    hero: {
+        alignSelf: "center",
+        fontFamily: "Lora",
+        fontSize: widthPercentageToDP("3"), 
+        fontWeight: "600",
+        flexDirection: "column",
+        marginTop: heightPercentageToDP("20")
+    },
+    description: {
+        textAlign: "center",
+        fontFamily: "Lora",
+        fontSize: widthPercentageToDP("3"), 
+        fontWeight: "600",
+        flexDirection: "column",
+        marginHorizontal: widthPercentageToDP("15")
+    },
+    buttonContainer: { 
+        fontSize: widthPercentageToDP("3"), 
+        fontWeight: "600",
+        flexDirection: "column",
+        marginHorizontal: widthPercentageToDP("40"),
+        marginVertical: heightPercentageToDP("5"),
+    },
+    button: {
+        backgroundColor: "#000",
+        textAlign: "center",
+        fontWeight: "600",
+        paddingVertical: heightPercentageToDP("5"),
+        paddingHorizontal: widthPercentageToDP("2"),
+        borderRadius: widthPercentageToDP("1"),
+        flexDirection: "column",
+    },
+    buttonText: {
+        color: "#fff",
+        fontFamily: "Lora",
+        fontSize: widthPercentageToDP("2"), 
+        fontWeight: "600",
+    },
 });
