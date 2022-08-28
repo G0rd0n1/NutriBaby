80d756f2cd9edf58464e5e2e99008185d2bb067a
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-08-28 13:36:08 +0200

    Changes

--- src/pages/Topics/index.tsx
+++ src/pages/Topics/index.tsx
@@ -33,16 +33,16 @@ const Topics = (): JSX.Element => {
   const renderTopics = (): JSX.Element => {
     return (
       <View style={styles.topicsContainer}>
-        <TouchableOpacity onPress={() => setGameMode("diabetes")}>
-          <Topic colour="#32ff7e" borderColour="#05c46b" text="Diabetes II" />
+        <TouchableOpacity onPress={() => setGameMode("nutrition")}>
+          <Topic colour="#32ff7e" borderColour="#05c46b" text="Nutrition" />
         </TouchableOpacity>
-        <Topic colour="#74b9ff" borderColour="#3c40c6" text="Anaemia" />
-        <Topic colour="#fffa65" borderColour="#ffd32a" text="Kwashiorkor" />
-        <Topic colour="#FDA7DF" borderColour="#ef5777" text="Hypertension" />
+        <Topic colour="#74b9ff" borderColour="#3c40c6" text="Mental health" />
+        <Topic colour="#fffa65" borderColour="#ffd32a" text="Physical health" />
+        <Topic colour="#FDA7DF" borderColour="#ef5777" text="Sexual health" />
       </View>
     );
   };
-
+  
   const renderLoading = (): JSX.Element => {
     return (
       <ActivityIndicator size="large" color="#000" style={styles.loading} />
